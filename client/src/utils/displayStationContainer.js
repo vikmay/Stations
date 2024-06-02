02164480409909360c9aeb3b482f9edffbfc07c7
Viktor Maikut <vikmaykut@gmail.com>
2024-06-02 23:46:42 +0300

    angular installed

--- client/src/utils/displayStationContainer.js
+++
@@ -1,5 +0,0 @@
-export function displayStationsContainer(listContainer) {
-    listContainer.innerHTML === '' ?
-        listContainer.style.display = 'none' :
-        listContainer.style.display = 'block';
-}
